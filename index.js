bd45c13eaf89ace67b6e436a1ff665ba22f359f2
seoljjang <seoljjang00@gmail.com>
2020-12-07 21:27:25 +0900

    db key 보호

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+const config = require("./config/key");
+
+//application/x-www-form-urlencodeed
+//위의 데이터를 분석해서 가져옴
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+//위의 타입으로 된 데이터를 분석해서 가져옴
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://seol:seol10314@boilerplate.nxxsu.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  //회원 가입 시 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터베이스에 넣어준다.
+  const user = new User(req.body);
+  //body-parser를 사용해 클라이언트로부터의 정보를 받아옴
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true
+    });
+  });
 });
 
 app.listen(port, () => {
